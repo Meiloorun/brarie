6bc3361502bec4f7dfb3fc7d373ff12615f4ccf4
Meiloorun <hishaam.k@live.co.uk>
2022-11-25 15:32:40 +0000

    started homeapp

--- brarie/settings.py
+++ brarie/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homeapp.apps.HomeappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
