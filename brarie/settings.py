8cf13a6e95cbb8a6dc89fbbfa58c417aacf8f984
Meiloorun <hishaam.k@live.co.uk>
2022-12-02 23:16:51 +0000

    migrations made, scss not working

--- brarie/settings.py
+++ brarie/settings.py
@@ -122,11 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR /'static/'
 SASS_PROCESSOR_ROOT = STATIC_ROOT
 
-STATICFILES_FINDERS = [
+STATICFILE_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.AppDirectoriesFinder',
     'sass_processor.finders.CssFinder',
