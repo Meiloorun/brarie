285a8c50fccc05637ec67fc085f8c4ac05c6b85f
Meiloorun <hishaam.k@live.co.uk>
2022-12-03 19:30:29 +0000

    musicapp models complete

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=128)
+    releasedate = models.DateTimeField()
+    artist = models.ManyToManyField('peopleapp.Person')
+    mode = models.IntegerField() #0 = Single, 1 = E.P., 3 = Album, 4 = Collection
+
+class Song(models.Model):
+    title = models.CharField(max_length=128)
+    artists = models.ManyToManyField('peopleapp.Person', related_name='artists')
+    producers = models.ManyToManyField('peopleapp.Person', related_name='producers')
+    writers = models.ManyToManyField('peopleapp.Person', related_name='songwriters')
+    length = models.IntegerField() #in minutes
+    Album = models.ForeignKey(Album, on_delete = models.CASCADE)
