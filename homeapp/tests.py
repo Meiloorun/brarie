267263b19af97c8e6cf036277b89ed53dfe41565
Meiloorun <hishaam.k@live.co.uk>
2022-12-05 04:23:11 +0000

    added a navbar and tests to homepage

--- homeapp/tests.py
+++ homeapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomePageTests(TestCase):
+    def setUp(self):
+        return
+
+    def test_homepage(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'homepage')
